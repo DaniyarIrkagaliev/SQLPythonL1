f9a509aceeb18d28c235689f96341b21496a6e06
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2024-06-19 18:07:19 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,10 +7,9 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-import db
 
 app = FastAPI()
-db.startDB()
+create()
 script_dir = os.path.dirname(__file__)
 app.mount("/static", StaticFiles(directory=os.path.join(script_dir, "static/")), name="static")
 
@@ -45,69 +44,69 @@ async def update_student(request: Request, textId: int, textEd1: str, textEd2: s
     )
     return await index(request)
 
-#
-# def create():
-#     try:
-#         create_db_query = "CREATE DATABASE IF NOT EXISTS test;"
-#         cur.execute(create_db_query)
-#
-#         create_files = ('CREATE TABLE `files` ('
-#                         '`id_file` int(11) NOT NULL,'
-#                         '`id_my` int(11) NOT NULL,'
-#                         '`description` text NOT NULL,'
-#                         '`name_origin` text NOT NULL,'
-#                         '`path` text NOT NULL,'
-#                         '`date_upload` text NOT NULL) ENGINE=InnoDB '
-#                         'DEFAULT CHARSET=cp1251;')
-#         cur.execute(create_files)
-#
-#         insert_val = (
-#             "INSERT INTO `files` (`id_file`, `id_my`, `description`, `name_origin`, `path`, `date_upload`) VALUES "
-#             "(16, 17, 'Закачка из менеджера', 'Стратегии в виртуальном футболе.pdf', "
-#             "'files/Стратегии в виртуальном футболе.pdf', '16-03-2022  08:41:22');")
-#
-#         cur.execute(insert_val)
-#
-#         create_myarttable = ("CREATE TABLE `myarttable` ("
-#                              "`id` int(11) NOT NULL,"
-#                              "`text` text NOT NULL,"
-#                              "`description` text NOT NULL,"
-#                              "`keywords` text NOT NULL)"
-#                              " ENGINE=InnoDB DEFAULT CHARSET=cp1251;")
-#
-#         cur.execute(create_myarttable)
-#
-#         insert_val = ("INSERT INTO `myarttable` (`id`, `text`, `description`, `keywords`) VALUES "
-#                       "(17, 'Baranov', 'Engeneer', 'Ivanov'), "
-#                       "(20, 'Fedorov', 'Cpp, Delphi, PHP, JS', '3t'), "
-#                       "(92, 'Daniel', 'Artist', 'Theater Saturday'), "
-#                       "(93, 'Andrew', 'Poet', 'First Electrotechnical University'), "
-#                       "(94, 'Nikita', 'Student', 'Technological Institute'), "
-#                       "(95, 'Ilya', 'Salesman', 'Hypermarket'), "
-#                       "(96, 'Matvey', 'Programmer', 'Metropolitan College'), "
-#                       "(97, 'Fedor', 'Loader', 'St. Petersburg State University'), "
-#                       "(98, 'Ivan', 'Student', 'LETI'), (99, 'Alexey', 'Engineer', 'ITMO');")
-#
-#         cur.execute(insert_val)
-#
-#         alter_table = "ALTER TABLE `files` ADD PRIMARY KEY (`id_file`), ADD KEY `id_my` (`id_my`);"
-#         cur.execute(alter_table)
-#
-#         alter_table = "ALTER TABLE `myarttable` ADD PRIMARY KEY (`id`);"
-#         cur.execute(alter_table)
-#
-#         alter_table = "ALTER TABLE `files` MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;"
-#         cur.execute(alter_table)
-#
-#         alter_table = "ALTER TABLE `myarttable` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;"
-#         cur.execute(alter_table)
-#
-#         alter_table = "ALTER TABLE `files`ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`id_my`) REFERENCES `myarttable` (`id`);"
-#         cur.execute(alter_table)
-#         myconn.commit()
-#
-#     except Error as e:
-#         print(e)
+
+def create():
+    try:
+        create_db_query = "CREATE DATABASE IF NOT EXISTS test;"
+        cur.execute(create_db_query)
+
+        create_files = ('CREATE TABLE `files` ('
+                        '`id_file` int(11) NOT NULL,'
+                        '`id_my` int(11) NOT NULL,'
+                        '`description` text NOT NULL,'
+                        '`name_origin` text NOT NULL,'
+                        '`path` text NOT NULL,'
+                        '`date_upload` text NOT NULL) ENGINE=InnoDB '
+                        'DEFAULT CHARSET=cp1251;')
+        cur.execute(create_files)
+
+        insert_val = (
+            "INSERT INTO `files` (`id_file`, `id_my`, `description`, `name_origin`, `path`, `date_upload`) VALUES "
+            "(16, 17, 'Закачка из менеджера', 'Стратегии в виртуальном футболе.pdf', "
+            "'files/Стратегии в виртуальном футболе.pdf', '16-03-2022  08:41:22');")
+
+        cur.execute(insert_val)
+
+        create_myarttable = ("CREATE TABLE `myarttable` ("
+                             "`id` int(11) NOT NULL,"
+                             "`text` text NOT NULL,"
+                             "`description` text NOT NULL,"
+                             "`keywords` text NOT NULL)"
+                             " ENGINE=InnoDB DEFAULT CHARSET=cp1251;")
+
+        cur.execute(create_myarttable)
+
+        insert_val = ("INSERT INTO `myarttable` (`id`, `text`, `description`, `keywords`) VALUES "
+                      "(17, 'Baranov', 'Engeneer', 'Ivanov'), "
+                      "(20, 'Fedorov', 'Cpp, Delphi, PHP, JS', '3t'), "
+                      "(92, 'Daniel', 'Artist', 'Theater Saturday'), "
+                      "(93, 'Andrew', 'Poet', 'First Electrotechnical University'), "
+                      "(94, 'Nikita', 'Student', 'Technological Institute'), "
+                      "(95, 'Ilya', 'Salesman', 'Hypermarket'), "
+                      "(96, 'Matvey', 'Programmer', 'Metropolitan College'), "
+                      "(97, 'Fedor', 'Loader', 'St. Petersburg State University'), "
+                      "(98, 'Ivan', 'Student', 'LETI'), (99, 'Alexey', 'Engineer', 'ITMO');")
+
+        cur.execute(insert_val)
+
+        alter_table = "ALTER TABLE `files` ADD PRIMARY KEY (`id_file`), ADD KEY `id_my` (`id_my`);"
+        cur.execute(alter_table)
+
+        alter_table = "ALTER TABLE `myarttable` ADD PRIMARY KEY (`id`);"
+        cur.execute(alter_table)
+
+        alter_table = "ALTER TABLE `files` MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;"
+        cur.execute(alter_table)
+
+        alter_table = "ALTER TABLE `myarttable` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;"
+        cur.execute(alter_table)
+
+        alter_table = "ALTER TABLE `files`ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`id_my`) REFERENCES `myarttable` (`id`);"
+        cur.execute(alter_table)
+        myconn.commit()
+
+    except Error as e:
+        print(e)
 
 
 def getStudents():
